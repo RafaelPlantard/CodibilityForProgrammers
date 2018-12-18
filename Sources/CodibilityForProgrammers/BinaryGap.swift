1b71660332e4f57d11716d9cbe1fdd62b2315374
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-12-18 06:38:41 -0200

    Fix implementation for really big binary representation

--- Sources/CodibilityForProgrammers/BinaryGap.swift
+++ Sources/CodibilityForProgrammers/BinaryGap.swift
@@ -7,10 +7,8 @@
 
 final class BinaryGap {
     func solution(_ n: Int) -> Int {
-        let binaryReversed = String(n, radix: 2, uppercase: true).reversed()
-        let binary = String(binaryReversed)
-        let binaryReversedAsInt = Int(binary) ?? 0
-        let binaryWithoutZerosAfterLastOne = String("\(binaryReversedAsInt)".reversed())
+        let binaryReversed = String(n, radix: 2, uppercase: true).reversed().drop(while: { char in char == "0" })
+        let binaryWithoutZerosAfterLastOne = String(binaryReversed.reversed())
         let numbersOfOnes = binaryWithoutZerosAfterLastOne.filter({ character in character == "1" }).count
 
         guard numbersOfOnes >= 2, binaryWithoutZerosAfterLastOne.count != numbersOfOnes,
